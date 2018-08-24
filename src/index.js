73b86026ded32440724f84b2f9ec006fb570d658
Joshua Russell <jkrussell756@gmail.com>
2018-08-24 04:19:23 -0400

    Got rid of useless comments in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-// Import dependencies
-// Import react-bootstrap
-// import 'bootstrap/dist/css/bootstrap.css';
+// Import dependencies 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
