ced6bc63362917688510f50eea0de5f30d5b6165
Joshua Russell <jkrussell756@gmail.com>
2018-08-24 03:37:26 -0400

    added react-bootstrap dependency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// Import dependencies
+// Import react-bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
